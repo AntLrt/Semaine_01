6daeeddf8b7b36d6d3c932010ee75a771a6cdb5f
AntLrt <loriot.antoine.pro@gmail.com>
2020-11-10 13:50:32 +0100

    Update SQL eval

--- SQL/Eval sql I.sql
+++ SQL/Eval sql I.sql
@@ -1,37 +1,37 @@
-CREATE DATABASE HOSTEL;
+CREATE DATABASE HOTEL;
 
-CREATE TABLE patron(
-ptr_num INT NOT NULL,
-ptr_nom VARCHAR(50) NOT NULL,
-ptr_adresse VARCHAR(50) NOT NULL,
-ptr_tel VARCHAR(30) NOT NULL
-,CONSTRAINT patron_PK PRIMARY KEY (ptr_num)
+CREATE TABLE oclient(
+cli_num INT NOT NULL,
+cli_nom VARCHAR(50) NOT NULL,
+cli_adresse VARCHAR(50) NOT NULL
+cli_tel VARCHAR(30) NOT NULL
+,CONSTRAINT oclient_PK PRIMARY KEY (cli_num)
 );
 
-CREATE TABLE order(
-ordr_num INT NOT NULL,
-ptr_num INT NOT NULL,
-ordr_date DATETIME NOT NULL,
-ordr_obs VARCHAR(50) NOT NULL
-,CONSTRAINT order_PK PRIMARY KEY (ordr_num)
-,CONSTRAINT order_patron_FK FOREIGN KEY (ptr_num) REFERENCES patron (cli-num)
+CREATE TABLE commande(
+com_num INT NOT NULL,
+cli_num INT NOT NULL,
+com_date DATETIME NOT NULL,
+com_obs VARCHAR(50) NOT NULL
+,CONSTRAINT commande_PK PRIMARY KEY (com_num)
+,CONSTRAINT commande_oclient_FK FOREIGN KEY (cli_num) REFERENCES oclient (cli-num)
 );
 
-CREATE TABLE product(
-prd_num INT NOT NULL,
+CREATE TABLE produit(
+pro_num INT NOT NULL,
 pro_libelle VARCHAR(50) NOT NULL,
 pro_description VARCHAR(50) NOT NULL
-,CONSTRAINT product PRIMARY KEY (pro_num)
+,CONSTRAINT produit PRIMARY KEY (pro_num)
 );
 
 CREATE TABLE est_compose(
-        ordr_num Int NOT NULL ,
+        com_num Int NOT NULL ,
         pro_num Int NOT NULL ,
-        est_qte INT NOT NULL 
-    ,CONSTRAINT PRIMARY KEY (ordr_num,pro_num)
-    ,CONSTRAINT est_compose_order_FK FOREIGN KEY(ordr_num) REFERENCES order(ordr_num)
-    ,CONSTRAINT est_compose_product_FK FOREIGN KEY(pro_num) REFERENCES product(pro_num)
+        est_qte INT NOT NULL
+    ,CONSTRAINT PRIMARY KEY (com_num,pro_num)
+    ,CONSTRAINT est_compose_commande_FK FOREIGN KEY(com_num) REFERENCES commande(com_num)
+    ,CONSTRAINT est_compose_produit_FK FOREIGN KEY(pro_num) REFERENCES produit(pro_num)
 );
 
 
-CREATE INDEX INDEX1 ON patron(ptr_nom)
+CREATE INDEX INDEX1 ON oclient(cli_nom)
