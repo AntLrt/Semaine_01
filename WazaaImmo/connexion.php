f0b069a57bf71cf1731292e66da6ac256c7de4e1
AntLrt <71831014+AntLrt@users.noreply.github.com>
2021-01-14 14:56:45 +0100

    Delete WazaaImmo directory

--- WazaaImmo/connexion.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-$liaison = mysqli_connect('127.0.0.1', 'root', '') or die ('Accès à la base de donnée impossible !!!');
-mysqli_select_db($liaison, 'waz_immo') or die ('Accès à la base de donnée impossible !!!');
-?>
